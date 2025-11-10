d004aa5f734ef045fcb11103d669ab7388beafa6
GouTouZhao <zhaoziteng0624@gmail.com>
2025-11-11 01:14:25 +0800

    请求头的不同版本兼容问题

--- main.cpp
+++ main.cpp
@@ -10,11 +10,19 @@
 
 static void addCorsHeaders(QHttpServerResponse& res)
 {
-    QHttpHeaders headers = res.headers();
-    headers.replaceOrAppend(QHttpHeaders::WellKnownHeader::AccessControlAllowOrigin, "*");
-    headers.replaceOrAppend(QHttpHeaders::WellKnownHeader::AccessControlAllowMethods, "GET, POST, OPTIONS");
-    headers.replaceOrAppend(QHttpHeaders::WellKnownHeader::AccessControlAllowHeaders, "Content-Type");
-    res.setHeaders(headers);
+#if QT_VERSION >= QT_VERSION_CHECK(6, 10, 0)
+    // Qt 6.10 及以上版本：直接使用 QHttpHeaders API
+    QHttpHeaders corsHeaders;
+    corsHeaders.replaceOrAppend(QHttpHeaders::WellKnownHeader::AccessControlAllowOrigin, "*");
+    corsHeaders.replaceOrAppend(QHttpHeaders::WellKnownHeader::AccessControlAllowMethods, "GET, POST, OPTIONS");
+    corsHeaders.replaceOrAppend(QHttpHeaders::WellKnownHeader::AccessControlAllowHeaders, "Content-Type, Authorization");
+    res.setHeaders(corsHeaders);
+#else
+    // Qt 6.4 等旧版本：使用基本字符串接口
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+#endif
 }
 
 int main(int argc, char* argv[])
